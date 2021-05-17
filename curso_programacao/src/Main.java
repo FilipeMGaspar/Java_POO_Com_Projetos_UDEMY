e769247f0509d7a5e9dbbb46d9b46ca1c63f07b6
filipe gaspar <fipewmc2@gmail.com>
2021-05-17 20:18:34 +0100

    Aula 18. Primeiro programa em Java no Eclipse

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Olá Mundo!");
 		System.out.println("Ufa! Estou livre da maldição.");
+		System.out.println("Este é o meu primeiro programa em Java!");
 	}
 
 }
