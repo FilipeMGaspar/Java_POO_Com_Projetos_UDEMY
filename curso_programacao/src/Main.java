3c9fba613ccb2939811f2cee0348f049e3f87e93
filipe gaspar <fipewmc2@gmail.com>
2021-05-22 10:41:10 +0100

    Aula 24. Saída de dados em Java

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -4,23 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.out.print("Olá Mundo!");
-		//System.out.println("Ufa! Estou livre da maldição.");
-		//System.out.println("Este é o meu primeiro programa em Java!");
-		//System.out.print(" Bom Dia!");
+		System.out.println("Olá Mundo!");
+		System.out.println("Ufa! Estou livre da maldição.");
+		System.out.println("Este é o meu primeiro programa em Java! \n");
+		System.out.print("Olá Mundo!");
+		System.out.print(" Bom Dia! \n");
 		
-		//int y = 32;
-		//double x = 10.35784;
+		int y = 32;
+		double x = 10.35784;
 		
-		//System.out.println(y);
-		//System.out.println(x);
-		//System.out.printf("%.2f \n", x);
-		//System.out.printf("%.4f \n", x);
-		//System.out.println("Bom dia!");
-		//Locale.setDefault(Locale.US);
-		//System.out.printf("%.4f \n", x);
-		//System.out.println("RESULTADO = " + x + " METROS");
-		//System.out.printf("Resultado %.2f Metros \n", x);
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f \n", x);
+		System.out.printf("%.4f \n", x);
+		System.out.println("Bom dia!");
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f \n", x);
+		System.out.println("RESULTADO = " + x + " METROS");
+		System.out.printf("Resultado %.2f Metros \n\n", x);
 		
 		String nome = "Maria";
 		int idade = 31;
