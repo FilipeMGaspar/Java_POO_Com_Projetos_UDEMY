5483c72ec2e8ec80573e4e71dee2386779babd63
filipe gaspar <fipewmc2@gmail.com>
2021-05-22 10:13:49 +0100

    Aula 24. Saída de dados em Java

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá Mundo!");
-		System.out.println("Ufa! Estou livre da maldição.");
-		System.out.println("Este é o meu primeiro programa em Java!");
+		//System.out.println("Ufa! Estou livre da maldição.");
+		//System.out.println("Este é o meu primeiro programa em Java!");
+		System.out.println("Bom Dia!");
 	}
 
 }
