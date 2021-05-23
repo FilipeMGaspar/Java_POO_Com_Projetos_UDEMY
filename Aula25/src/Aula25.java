6cf979b72fbe7427bcecd5ee9289ea6c8c80a185
filipe gaspar <fipewmc2@gmail.com>
2021-05-23 08:43:25 +0100

    25. Processamento de dados em Java, Casting

--- Aula25/src/Aula25.java
+++ Aula25/src/Aula25.java
@@ -3,6 +3,21 @@ public class Aula25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/*
+		 //Exemplo 1
+		int x,y;		
+		
+		x = 5;
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		*/
+		
+		
+		/*
+	 	//Exemplo 2
 		int x;
 		double y;
 		
@@ -11,6 +26,11 @@ public class Aula25 {
 		
 		System.out.println(x);
 		System.out.println(y);
+		*/
+		
+		//Exemplo 3
+		
+		
 	}
 
 }
