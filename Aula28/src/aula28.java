506f28253ca269822c852b2f30044cb2ba9e2663
filipe gaspar <fipewmc2@gmail.com>
2021-05-30 20:26:51 +0100

    Aula 28. Funções matemáticas em Java

--- Aula28/src/aula28.java
+++ Aula28/src/aula28.java
@@ -17,11 +17,15 @@ public class aula28 {
 	
 		A = Math.pow(x, y);
 		B = Math.pow(x, 2.0);
-		C = Math.pow(5.0, 2.0);
-		
+		C = Math.pow(5.0, 2.0);		
 		System.out.println(x + " elevado a " + y + " = " + A);
 		System.out.println(x + " elevado ao quadrado = " + B);
 		System.out.println(5 + " elevado ao quadrado = " + C);
+		
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
 	}
 
 }
