e31e1a286a4c2df08bb19b76d188ed8e76a977d6
filipe gaspar <fipewmc2@gmail.com>
2021-05-30 19:15:09 +0100

    Aula 26. Entrada de dados em Java - Parte 1

--- Aula26/src/aula26.java
+++ Aula26/src/aula26.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class aula26 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in); //Leitura de dados 
+		
 	}
 
 }
