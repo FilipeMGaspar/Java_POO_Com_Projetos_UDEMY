2218fedcaf14ddab3d92cc251437b133d914ac7d
filipe gaspar <fipewmc2@gmail.com>
2021-05-30 19:58:05 +0100

    Aula 27. Entrada de dados em Java - Parte 2

--- Aula27/src/aula27.java
+++ Aula27/src/aula27.java
@@ -8,6 +8,12 @@ public class aula27 {
 		Scanner sc = new Scanner(System.in);
 		
 		String s1, s2, s3;
+		
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("Dados Digitados");
 	}
 
 }
