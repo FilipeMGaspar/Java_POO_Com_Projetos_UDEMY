e66ec9e4753f83ff8aeaec838023dd70573cad55
filipe gaspar <fipewmc2@gmail.com>
2021-05-30 20:16:07 +0100

    Aula 28. Funções matemáticas em Java

--- Aula28/src/aula28.java
+++ Aula28/src/aula28.java
@@ -3,7 +3,12 @@ public class aula28 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		
+		
 	}
 
 }
