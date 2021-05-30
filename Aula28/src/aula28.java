fe4862a045a9e6847c2948321f086043062b8d83
filipe gaspar <fipewmc2@gmail.com>
2021-05-30 20:21:31 +0100

    Aula 28. Funções matemáticas em Java

--- Aula28/src/aula28.java
+++ Aula28/src/aula28.java
@@ -8,6 +8,12 @@ public class aula28 {
 		double z = -5.0;
 		double A, B, C;
 		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = "+ A);
+		System.out.println("Raiz quadrada de " + y + " = "+ B);
+		System.out.println("Raiz quadrada de 25" + x + " = "+ C);
 		
 	}
 
