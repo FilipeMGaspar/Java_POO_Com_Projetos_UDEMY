1eba7ba1edb5f5579f92f6ce96199ef697d6ef24
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 21:40:52 +0100

    Aula 30. Exercício 04

--- Aula30/src/Exercicio04.java
+++ Aula30/src/Exercicio04.java
@@ -1,16 +1,18 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
-		
+		Scanner teclado = new Scanner(System.in);
 		System.out.println("Exercicio 04 - Salário do funcionário!");
 		
 		int numFunc;
 		
 		System.out.print("Número Do funcionário: ");
+		numFunc = teclado.nextInt();
 	}
 
 }
