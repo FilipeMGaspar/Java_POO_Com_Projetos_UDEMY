cb3a48582c47799b2eee4bc4e9716daa2667c0d2
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 21:43:42 +0100

    Aula 30. Exercício 04

--- Aula30/src/Exercicio04.java
+++ Aula30/src/Exercicio04.java
@@ -10,11 +10,17 @@ public class Exercicio04 {
 		System.out.println("Exercicio 04 - Salário do funcionário!");
 		
 		int numFunc, horasTrab;
+		double valorHora;
 		
 		System.out.print("Número Do funcionário: ");
 		numFunc = teclado.nextInt();
 		System.out.print("Horas trabalhadas: ");
 		horasTrab = teclado.nextInt();
+		System.out.println("Valor a pagar por Hora: ");
+		valorHora = teclado.nextDouble();
+
+		
+		teclado.close();
 	}
 
 }
