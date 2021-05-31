05efe46e7886a127f358f7a0ee88c8f5ffd85d58
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 21:01:21 +0100

    Aula 30. Exercício 01

--- Aula30/src/Exercicio01.java
+++ Aula30/src/Exercicio01.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 
 public class Exercicio01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		
 		System.out.println("Exercicio 01 - Soma de dois Números!");
 		int num1, num2;
 		
+		
 		System.out.print("Primeiro número: ");
+		num1 = teclado.nextInt();
 		System.out.println("Segundo número: ");
+		num2 = teclado.nextInt();
+		
+		teclado.close();
 	}
 
 }
