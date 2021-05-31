9261cec003d20513bab9640422d874aad76f57d0
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 20:10:59 +0100

    Aula 30. Exercício 01

--- Aula30/src/Exercicio01.java
+++ Aula30/src/Exercicio01.java
@@ -1,9 +1,8 @@
 
-public class Aula30 {
+public class Exercicio01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Exercicio 01
 		System.out.println("Exercicio 01 - Soma de dois Números!");
 	}
 
