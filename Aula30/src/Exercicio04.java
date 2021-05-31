16357e5be32eb04a08b8b1d15f86f8b26309507b
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 21:38:30 +0100

    Aula 30. Exercício 04

--- Aula30/src/Exercicio04.java
+++ Aula30/src/Exercicio04.java
@@ -3,7 +3,8 @@ public class Exercicio04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Exercicio 04 - Salário do funcionário!");
+		
 	}
 
 }
