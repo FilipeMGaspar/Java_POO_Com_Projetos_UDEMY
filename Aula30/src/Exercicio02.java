b598743bddf5e7e3e477be7e154566f4e3f307aa
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 21:05:29 +0100

    Aula 30. Exercício 02

---
+++ Aula30/src/Exercicio02.java
@@ -0,0 +1,13 @@
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Exercicio 02 - Área do círculo!");
+		
+		
+	}
+
+}
