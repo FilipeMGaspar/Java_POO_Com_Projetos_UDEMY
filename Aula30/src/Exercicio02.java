73dd56327d5340950941c0e65dd6ad8f54efe829
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 21:20:42 +0100

    Aula 30. Exercício 02

--- Aula30/src/Exercicio02.java
+++ Aula30/src/Exercicio02.java
@@ -1,17 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Exercicio 02 - Área do círculo!");
+		
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
 		
 		double PI = 3.14159;
 		double raio, area;
 		
-		System.out.println("Exercicio 02 - Área do círculo!");
-		
 		System.out.println("Valor do raio: ");
 		
-		
+		teclado.close();
 	}
 
 }
