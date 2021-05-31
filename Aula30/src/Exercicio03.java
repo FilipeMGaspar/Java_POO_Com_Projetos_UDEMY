3d2edd66a5af975360bd9ad99cfb52c8fbbb6d79
filipe gaspar <fipewmc2@gmail.com>
2021-05-31 21:31:08 +0100

    Aula 30. Exercício 03

--- Aula30/src/Exercicio03.java
+++ Aula30/src/Exercicio03.java
@@ -8,6 +8,9 @@ public class Exercicio03 {
 		int a, b, c, d;
 		
 		System.out.println("Primeiro número: ");
+		System.out.println("Segundo número: ");
+		System.out.println("Terceiro número: ");
+		System.out.println("Quarto número: ");
 		
 	}
 
