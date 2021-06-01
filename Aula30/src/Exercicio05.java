c04e4be0f09f27ef138c8b0b66dcaeabecd73122
filipe gaspar <fipewmc2@gmail.com>
2021-06-01 21:54:25 +0100

    Aula 30. Exercício 05

--- Aula30/src/Exercicio05.java
+++ Aula30/src/Exercicio05.java
@@ -1,16 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
 		System.out.println("Exercicio 05 - Calculadora de valor a ser pago!");
 		
-		int codProd, qtProd;
+		int codProd1, qtProd1;
 		double valorUnP1, valorUnP2; 
 	
 		System.out.println("Produto 1: ");
 		System.out.print("   Código     ..: ");
+		codProd1 = teclado.nextInt();
 		System.out.print("   Quantidade ..: ");
+		qtProd1 = teclado.nextInt();
 		System.out.print("   Preço      ..: ");
 	}
 
