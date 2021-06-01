c6bc8b093cdb3ac13edc324ec68bf8f546e389c4
filipe gaspar <fipewmc2@gmail.com>
2021-06-01 21:46:09 +0100

    Aula 30. Exercício 05

--- Aula30/src/Exercicio05.java
+++ Aula30/src/Exercicio05.java
@@ -3,7 +3,7 @@ public class Exercicio05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Exercicio 05 - Calculadora de valor a ser pago!");
 	}
 
 }
