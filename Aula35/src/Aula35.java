d8a87b0f5e08141b716a683eb29351503f40594b
filipe gaspar <fipewmc2@gmail.com>
2021-06-08 12:09:00 +0100

    Aula 35. Estrutura condicional (if-else)

--- Aula35/src/Aula35.java
+++ Aula35/src/Aula35.java
@@ -3,7 +3,9 @@ public class Aula35 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Bom Dia!");
+		System.out.println("Boa Tarde!");
+		System.out.println("Boa Noite!");
 	}
 
 }
