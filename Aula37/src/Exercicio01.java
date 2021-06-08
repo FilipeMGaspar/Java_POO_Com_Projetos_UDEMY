097a2782311351d7ef77217a1335dd2e0d8a9ad3
filipe gaspar <fipewmc2@gmail.com>
2021-06-08 12:41:44 +0100

    Aula 37. Exercício 01

--- Aula37/src/Exercicio01.java
+++ Aula37/src/Exercicio01.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Exercicio01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		int num;
+		
+		System.out.print("Insira um valor numérico: ");
+		num = teclado.nextInt();
+		
+		teclado.close();
 	}
 
 }
