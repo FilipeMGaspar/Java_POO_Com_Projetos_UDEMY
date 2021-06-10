7d69fb960d9aae201067faa9111b66df1e4384ab
filipe gaspar <fipewmc2@gmail.com>
2021-06-10 12:14:00 +0100

    Aula 37. Exercício 02 - Par ou Ímpar

--- Aula37/src/Exercicio02.java
+++ Aula37/src/Exercicio02.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Exercicio02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.println("Indique um número: ");
+		num = teclado.nextInt();
+		
+		teclado.close();
 	}
 
 }
