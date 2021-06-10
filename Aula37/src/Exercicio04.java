05420bea0c5d86f006e412f750b2959cd4179866
filipe gaspar <fipewmc2@gmail.com>
2021-06-10 12:32:42 +0100

    Aula 37. Exercício 04 - Duração de um Jogo

--- Aula37/src/Exercicio04.java
+++ Aula37/src/Exercicio04.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class Exercicio04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner Teclado = new Scanner(System.in);
+		
+		int horaInicio, horaFim;
+		
+		
 	}
 
 }
