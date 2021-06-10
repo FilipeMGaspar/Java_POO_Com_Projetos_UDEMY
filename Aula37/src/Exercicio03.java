691576919b595e75d705ee36cac4f5da1fecb299
filipe gaspar <fipewmc2@gmail.com>
2021-06-10 12:25:19 +0100

    Aula 37. Exercício 03 - Serão Múltiplos

--- Aula37/src/Exercicio03.java
+++ Aula37/src/Exercicio03.java
@@ -7,11 +7,22 @@ public class Exercicio03 {
 		Scanner teclado = new Scanner(System.in);
 		
 		int num1, num2;
+		int div;
 		
-		System.out.println("Indique um número inteiro: ");
+		System.out.print("Indique um número inteiro: ");
 		num1 = teclado.nextInt();
-		System.out.println("Indique outro número inteiro: ");
+		System.out.print("Indique outro número inteiro: ");
 		num2 = teclado.nextInt();
+		
+		if(num1 > num2) {
+			div = num1 % num2;
+		}else {
+			div = num2 % num1;
+		}
+		
+		if(div == 0) {
+			System.out.println("Os números " + num1 + " e " + num2 +" São MULTIPLOS");
+		}
 		
 		teclado.close();
 	}
