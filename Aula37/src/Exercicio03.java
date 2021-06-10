e31603d6d2760531ddaf2bf9eeaeb605d1ac30e6
filipe gaspar <fipewmc2@gmail.com>
2021-06-10 12:20:52 +0100

    Aula 37. Exercício 03 - Serão Múltiplos

--- Aula37/src/Exercicio03.java
+++ Aula37/src/Exercicio03.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 
 public class Exercicio03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		
 		int num1, num2;
+		
+		
+		
+		teclado.close();
 	}
 
 }
