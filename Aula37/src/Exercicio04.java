497c37df0adf84e64b1f79a977b819b636e00cb8
filipe gaspar <fipewmc2@gmail.com>
2021-06-11 12:22:09 +0100

    Aula 37. Exercício 04 - Duração de um Jogo

--- Aula37/src/Exercicio04.java
+++ Aula37/src/Exercicio04.java
@@ -7,21 +7,21 @@ public class Exercicio04 {
 		Scanner teclado = new Scanner(System.in);
 		
 		int horaInicio, horaFim;
+		int duracao;
 		
 		System.out.print("Indique a hora de inicio do jogo: ");
 		horaInicio = teclado.nextInt();
 		System.out.print("Indique a hora de término do jogo: ");
 		horaFim = teclado.nextInt();
 		
-		if(horaInicio > horaFim) {
-			if(horaInicio - horaFim < 1) {
-				System.out.println("A duração do jogo deverá ser superior a uma 1 hora");
-			}
-		}else {
-			if((horaInicio - horaFim) * -1 < 1) {
-				System.out.println("A duração do jogo deverá ser superior a uma 1 hora");
-			}
+		duracao = horaInicio - horaFim;
+		
+		if(duracao < 0) {
+			duracao *= -1;
+		}else if(duracao == 0) {
+			duracao = 24;
 		}
+		System.out.println("Duracao " + duracao);
 		
 	}
 
