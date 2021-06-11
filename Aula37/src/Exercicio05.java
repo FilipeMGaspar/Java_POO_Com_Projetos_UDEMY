e425fa2125d694cbc15db918ba77f0ea09c14440
filipe gaspar <fipewmc2@gmail.com>
2021-06-11 12:34:40 +0100

    Aula 37. Exercício 05 - Conta a pagar

--- Aula37/src/Exercicio05.java
+++ Aula37/src/Exercicio05.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Exercicio05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		
+		int codProd;
+		double preco;
+		
+		System.out.println("| CODIGO \t| ESOECIFICAÇÃO \t| PREÇO |");
+		
+		teclado.close();
 	}
 
 }
