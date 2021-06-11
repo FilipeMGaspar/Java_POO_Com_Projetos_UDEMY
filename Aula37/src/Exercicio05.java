58f72939c129c3360a5f8f591a0f1ac778bc54bc
filipe gaspar <fipewmc2@gmail.com>
2021-06-11 12:43:36 +0100

    Aula 37. Exercício 05 - Conta a pagar

--- Aula37/src/Exercicio05.java
+++ Aula37/src/Exercicio05.java
@@ -16,6 +16,7 @@ public class Exercicio05 {
 		System.out.println("|    2    \t   X-Salada        \t   R$ 4.50  |");
 		System.out.println("|    3    \t   X-Bacon         \t   R$ 5.00  |");
 		System.out.println("|    4    \t   Torrada Simples \t   R$ 2.00  |");
+		System.out.println("|    4    \t   Refrigerante    \t   R$ 1.50  |");
 		teclado.close();
 	}
 
