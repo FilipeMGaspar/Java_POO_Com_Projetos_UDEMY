f3b4edda9053001191283757a2ab57aa92df835b
filipe gaspar <fipewmc2@gmail.com>
2021-06-12 08:58:03 +0100

    Aula 37. Exercício 06 - No Intervalo de Valores

--- Aula37/src/Exercicio06.java
+++ Aula37/src/Exercicio06.java
@@ -12,7 +12,7 @@ public class Exercicio06 {
 		System.out.print("Digite um valor: ");
 		num = teclado.nextDouble();
 		
-		if(num > 0 || num < 25) {
+		if(num > 0 && num < 25) {
 			intervalo = "Está no Intervalo entre [0 e 24]";
 		}else if(num < 50) {
 			intervalo = "Está no Intervalo entre [25 e 49]";
