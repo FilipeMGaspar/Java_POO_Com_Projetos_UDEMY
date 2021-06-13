fac85ac48e6730408b4c76d89a55e6657d2046ce
filipe gaspar <fipewmc2@gmail.com>
2021-06-13 13:30:25 +0100

    Aula 37. Exercício 07 - Coordenadas  no plano

--- Aula37/src/Exercicio07.java
+++ Aula37/src/Exercicio07.java
@@ -4,6 +4,7 @@ public class Exercicio07 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		double x, y;
 		
 		System.out.print("Coordenada no Eixo do X: ");
