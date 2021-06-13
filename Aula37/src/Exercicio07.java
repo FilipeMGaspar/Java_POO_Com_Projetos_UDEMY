a25fcf1a19f686b8275bb57a914e5d28fca7d65e
filipe gaspar <fipewmc2@gmail.com>
2021-06-13 13:33:17 +0100

    Aula 37. Exercício 07 - Coordenadas  no plano

--- Aula37/src/Exercicio07.java
+++ Aula37/src/Exercicio07.java
@@ -1,14 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio07 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
 		
 		double x, y;
 		
 		System.out.print("Coordenada no Eixo do X: ");
+		x = teclado.nextDouble();
 		System.out.print("Coordenada no Eixo do Y: ");
+		y = teclado.nextDouble();
+		
+		
+		teclado.close();
 	}
 
 }
