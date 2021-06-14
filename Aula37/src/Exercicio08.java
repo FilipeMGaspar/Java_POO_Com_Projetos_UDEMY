3cfb4f31e45c7d310b26d973f97609029d7e8536
filipe gaspar <fipewmc2@gmail.com>
2021-06-14 19:50:58 +0100

    Aula 37. Exercício 08 - Imposto de Renda

--- Aula37/src/Exercicio08.java
+++ Aula37/src/Exercicio08.java
@@ -1,10 +1,13 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio08 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
 		Scanner teclado = new Scanner(System.in);
+		
 		double salario;
 		
 		System.out.print("Salário Bruto: ");
