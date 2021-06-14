9b0a08ee9cfabd9f10fd1f19b2e8f227879db1b2
filipe gaspar <fipewmc2@gmail.com>
2021-06-14 21:49:31 +0100

    Aula 37. Exercício 08 - Imposto de Renda

--- Aula37/src/Exercicio08.java
+++ Aula37/src/Exercicio08.java
@@ -20,6 +20,9 @@ public class Exercicio08 {
 		}else if(salario <= 3000) {
 			imposto = 8; //Imposto será de 8%
 			valorImp = (salario - 2000 )* imposto /100;
+		}else if(salario <= 4500) {
+			imposto = 18; //Imposto será 18%
+			valorImp =((salario - 2000) * 8 /100) + ((salario - 3000) * imposto /100); 
 		}
 		
 		System.out.println("\nUma renda de R$ " + salario +"\n  Paga R$ "+ valorImp +" de imposto de renda");
