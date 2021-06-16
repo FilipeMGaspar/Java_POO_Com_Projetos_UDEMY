46c8ecf18183344e00edb23254affb1f50d267e0
filipe gaspar <fipewmc2@gmail.com>
2021-06-16 21:36:05 +0100

    Aula 38. Sintaxe opcional - operadores de atribuição cumulativa

--- Aula38/src/Aula38.java
+++ Aula38/src/Aula38.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Aula38 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		
 		int minutos;
 		
 	}
