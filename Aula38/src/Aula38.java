622a4d58072d2269522971dbed049ba79e175e7b
filipe gaspar <fipewmc2@gmail.com>
2021-06-16 21:45:00 +0100

    Aula 38. Sintaxe opcional - operadores de atribuição cumulativa

--- Aula38/src/Aula38.java
+++ Aula38/src/Aula38.java
@@ -7,13 +7,16 @@ public class Aula38 {
 		Scanner teclado = new Scanner(System.in);
 		
 		int minutos;
-		double pBase = 50;
+		double pBase = 50.0, pMintAdic = 2.0;
 		System.out.print("Indique os minutos consumidos: ");
 		minutos = teclado.nextInt();
 		
 		System.out.println("\nO valor da sua factura será de: ");
 		
-		if(minutos > 0 && minutos < 100) {
+		if(minutos > 0 && minutos <= 100) {
+			System.out.println("  R$ "+ pBase);
+		}else {
+			pBase = pBase + ((minutos - 100) * pMintAdic);
 			System.out.println("  R$ "+ pBase);
 		}
 	}
