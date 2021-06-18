7f48ad4e1a106d182ec06d1438b524711570e567
filipe gaspar <fipewmc2@gmail.com>
2021-06-18 08:20:32 +0100

    Aula 39. Sintaxe opcional - switch-case

--- Aula39/src/Aula39.java
+++ Aula39/src/Aula39.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 
 public class Aula39 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
+		Scanner teclado = new Scanner(System.in); 
+		
 		int diaSemana;
 		String dia;
+		teclado.close();
 	}
 
 }
