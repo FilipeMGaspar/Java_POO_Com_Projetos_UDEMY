67aa73c6ff4b32a921e45ae2fe9eef74db50fd85
filipe gaspar <fipewmc2@gmail.com>
2021-06-18 08:40:27 +0100

    Aula 40. Expressão condicional ternária

--- Aula40/src/Aula40.java
+++ Aula40/src/Aula40.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Aula40 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		double preco;
 	}
 
 }
