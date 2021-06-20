1a9d7d65ee2c2ddf0bbcaaa893a575db0393b32c
filipe gaspar <fipewmc2@gmail.com>
2021-06-20 08:30:59 +0100

    Aula 41. Escopo e inicialização

--- Aula41/src/Aula_41.java
+++ Aula41/src/Aula_41.java
@@ -4,7 +4,7 @@ public class Aula_41 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double price = 400.00;
-		double discount;
+		double discount = 0.0;
 		
 		if(price < 200.00) {
 			discount = price * 0.1;
