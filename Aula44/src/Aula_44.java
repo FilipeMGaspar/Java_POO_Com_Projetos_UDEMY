6e00b1c1c87cd45652ea32e419c0186b9a7de90e
filipe gaspar <fipewmc2@gmail.com>
2021-06-20 08:47:17 +0100

    Aula 44. Como utilizar o DEBUG no Eclipse (execução passo a passo)

--- Aula44/src/Aula_44.java
+++ Aula44/src/Aula_44.java
@@ -1,9 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Aula_44 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
 		
+		double largura;
+		double comprimento;
+		double metroQuadrado;
+		
+		
+		
+		teclado.close();
 	}
 
 }
