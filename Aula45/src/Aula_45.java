a1bc18d5bfb071a581ae6b387167d63bea281b4b
filipe gaspar <fipewmc2@gmail.com>
2021-06-24 11:19:21 +0100

    Aula 45. Estrutura repetitiva enquanto (while)

--- Aula45/src/Aula_45.java
+++ Aula45/src/Aula_45.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 
 public class Aula_45 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num;
+		Scanner teclado = new Scanner(System.in);
 		
+		int num;
+		System.out.println("Digite um número: ");
+		num = teclado.nextInt();
+		
+		while (num != 0) {
+			
+		}
+		teclado.close();
 	}
 
 }
