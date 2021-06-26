ffe223673f0fc82493eae1115213b87df4d8e1f6
filipe gaspar <fipewmc2@gmail.com>
2021-06-26 10:48:27 +0100

    Aula 49. Exercício 01

--- Aula49/src/Exercicio01.java
+++ Aula49/src/Exercicio01.java
@@ -3,7 +3,13 @@ public class Exercicio01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		int senha = 2002;
+		int pass;
+		
+		while (pass != senha) {
+			
+		}
 	}
 
 }
