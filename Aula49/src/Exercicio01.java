6ff351fae5654af3d6f29a81044f55bf87198972
filipe gaspar <fipewmc2@gmail.com>
2021-06-26 10:49:26 +0100

    Aula 49. Exercício 01

--- Aula49/src/Exercicio01.java
+++ Aula49/src/Exercicio01.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
 
 public class Exercicio01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		Scanner teclado = new Scanner(System.in);
 		int senha = 2002;
 		int pass;
 		
 		while (pass != senha) {
 			
 		}
+		
+		teclado.close();
 	}
 
 }
