3b52c5e021b55827b65df91212c5f77f55226cf0
filipe gaspar <fipewmc2@gmail.com>
2021-06-26 10:58:57 +0100

    Aula 49. Exercício 02

--- Aula49/src/Exercicio02.java
+++ Aula49/src/Exercicio02.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
 
 public class Exercicio02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		double x, y;
 		
 		System.out.print("Coordenada X = ");
 		System.out.print("Coordenada Y = ");
+		
+		teclado.close();
 	}
 
 }
