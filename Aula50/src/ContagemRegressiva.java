89730fa8e3babdc822784e1fb1ba76df39e87f2f
filipe gaspar <fipewmc2@gmail.com>
2021-07-03 19:36:46 +0100

    Aula 50. Estrutura repetitiva para (for)

--- Aula50/src/ContagemRegressiva.java
+++ Aula50/src/ContagemRegressiva.java
@@ -3,7 +3,9 @@ public class ContagemRegressiva {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int i = 4; i >= 0; i--) {
+			System.out.println("Valor do i: " + i);
+		}
 	}
 
 }
