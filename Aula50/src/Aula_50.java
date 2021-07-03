3144d6c5f8253dfdc874e5ec450f0a9979aab53f
filipe gaspar <fipewmc2@gmail.com>
2021-07-03 19:17:45 +0100

    Aula 50. Estrutura repetitiva para (for)

--- Aula50/src/Aula_50.java
+++ Aula50/src/Aula_50.java
@@ -5,13 +5,14 @@ public class Aula_50 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
-		int qtDeNum, num;
+		int qtDeNum, num, soma = 0;
 		
 		System.out.print("Quantos números deseja somar? ");
 		qtDeNum = teclado.nextInt();		
 		for(int i = 1; i <= qtDeNum; i++) {
 			System.out.print("\nNúmero " + i + " de  " + qtDeNum + ": ");
 			num = teclado.nextInt();
+			soma += num;
 		}
 		
 		teclado.close();
