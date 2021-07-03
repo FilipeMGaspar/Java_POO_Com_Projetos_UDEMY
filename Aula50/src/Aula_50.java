cccbe86399c37326cab13b34a9941789293a89b2
filipe gaspar <fipewmc2@gmail.com>
2021-07-03 19:12:26 +0100

    Aula 50. Estrutura repetitiva para (for)

--- Aula50/src/Aula_50.java
+++ Aula50/src/Aula_50.java
@@ -3,7 +3,9 @@ public class Aula_50 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int qtDenum, num;
+		
+		System.out.print("Quantos números deseja somar? ");
 	}
 
 }
