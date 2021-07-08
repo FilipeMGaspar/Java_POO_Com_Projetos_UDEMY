92f2336c8949d2f75e80cc27e9c90a159d99fe3e
filipe gaspar <fipewmc2@gmail.com>
2021-07-08 11:46:15 +0100

    Aula 55. Estrutura repetitiva faça-enquanto (do-while)

--- Aula55/src/Aula_55.java
+++ Aula55/src/Aula_55.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 
 public class Aula_55 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		double tempCels;
 		double temFahr;
+		
+		teclado.close();
 	}
 
 }
