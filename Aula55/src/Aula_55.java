3b312fd6b5950e8dbc615f439121c4921c88441f
filipe gaspar <fipewmc2@gmail.com>
2021-07-08 11:56:47 +0100

    Aula 55. Estrutura repetitiva faça-enquanto (do-while)

--- Aula55/src/Aula_55.java
+++ Aula55/src/Aula_55.java
@@ -13,7 +13,7 @@ public class Aula_55 {
 			System.out.print("Digite a temperatura em Celsius: ");
 			tempCels = teclado.nextDouble();
 			temFahr = (9 * tempCels / 5) + 32;
-			System.out.println("");
+			System.out.println("Equivalente em Fahrenheit: " + temFahr + "\n");
 			System.out.print("Desja repetir (s/n)? ");
 			resp = teclado.next().charAt(0);
 		}while(resp != 'n' && resp != 'N');
