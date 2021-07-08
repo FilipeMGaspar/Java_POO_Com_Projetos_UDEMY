29dd7e58d6075fe53a41944226bea0b26c8194e1
filipe gaspar <fipewmc2@gmail.com>
2021-07-08 11:51:36 +0100

    Aula 55. Estrutura repetitiva faça-enquanto (do-while)

--- Aula55/src/Aula_55.java
+++ Aula55/src/Aula_55.java
@@ -11,7 +11,7 @@ public class Aula_55 {
 		
 		do {
 			System.out.print("Desja repetir (s/n)? ");
-			
+			resp = teclado.next().charAt(0);
 		}while(resp != 'n' || resp != 'N');
 		teclado.close();
 	}
