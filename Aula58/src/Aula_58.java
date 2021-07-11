508cf3f9fbedc45649417f8c8656d302edaf3e0e
filipe gaspar <fipewmc2@gmail.com>
2021-07-11 08:16:46 +0100

    Aula 58. Operadores bitwise

--- Aula58/src/Aula_58.java
+++ Aula58/src/Aula_58.java
@@ -6,9 +6,10 @@ public class Aula_58 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
+		int mask = 0b100000;
 		int n = sc.nextInt();
 		
-		if() {
+		if((n & mask) != 0) {
 			System.out.println("6º bit é verdadeiro");
 		}else {
 			System.out.println("6º bit é falso");
