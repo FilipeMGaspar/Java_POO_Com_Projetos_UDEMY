559c0494e40dbe717ee43ee9b4928fa2b35011bc
filipe gaspar <fipewmc2@gmail.com>
2021-07-11 08:21:25 +0100

    Aula 59. Funções interessantes para String

--- Aula59/src/Aula_59.java
+++ Aula59/src/Aula_59.java
@@ -4,6 +4,9 @@ public class Aula_59 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String original = "abcde FGHIJ ABC abc DEFG";
+		
+	    String s01 = original.toLowerCase();
+	    System.out.println(s01);
 	}
 
 }
