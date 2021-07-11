720d6c90dd3d108fec2f7c90c54624bb2947cca3
filipe gaspar <fipewmc2@gmail.com>
2021-07-11 08:12:42 +0100

    Aula 58. Operadores bitwise

--- Aula58/src/Aula_58.java
+++ Aula58/src/Aula_58.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class Aula_58 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		sc.close();
 	}
 
 }
