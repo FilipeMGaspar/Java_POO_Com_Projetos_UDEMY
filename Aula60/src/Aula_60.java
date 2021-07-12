34002815924feb8da17828a2e37b7473e11ba00e
filipe gaspar <fipewmc2@gmail.com>
2021-07-12 23:15:47 +0100

    Aula 60. Comentários em Java (básico)

--- Aula60/src/Aula_60.java
+++ Aula60/src/Aula_60.java
@@ -1,9 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Aula_60 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double a, b, c, delta;
+		System.out.println("Digite os valores dos coeficientes:");
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		c = sc.nextDouble();
+		delta = b * b - 4 * a * c; // cálculo do valor de delta
+		sc.close();
 	}
 
 }
