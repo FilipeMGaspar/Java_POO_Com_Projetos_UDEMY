80dc5dee80485a4c2e54e6566ceb5d97959e57af
filipe gaspar <fipewmc2@gmail.com>
2021-07-14 23:07:00 +0100

    Exercicio-02 - Adivinha o número.

--- Exercicios/src/Exercicio02.java
+++ Exercicios/src/Exercicio02.java
@@ -5,8 +5,7 @@ public class Exercicio02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
-		int num = (int)(Math.random() * 10);
-		System.out.println("Aleatorio: " + num);
+		int num = (int)(Math.random() * 10);		
 		
 		teclado.close();
 	}
