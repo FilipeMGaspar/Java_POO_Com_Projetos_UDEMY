e1c4dd17e7c632d5186b5ca3c8999b650299ca38
filipe gaspar <fipewmc2@gmail.com>
2021-07-14 23:14:06 +0100

    Exercicio-02 - Adivinha o número.

--- Exercicios/src/Exercicio02.java
+++ Exercicios/src/Exercicio02.java
@@ -5,11 +5,14 @@ public class Exercicio02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
-		int tenta = 1;
+		int tenta = 1, palpite;
 		int num = (int)(Math.random() * 10);
 		
 		while(tenta < 4) {
+			System.out.println("\n-----------------------------");
 			System.out.println("Tentativa " + tenta + " de 3");
+			System.out.print("  ..: Palpite: " );
+			palpite = teclado.nextInt();
 			tenta++;
 		}
 		
