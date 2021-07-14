cca033a7db0756c9b934062b0722f3614c327963
filipe gaspar <fipewmc2@gmail.com>
2021-07-14 23:19:38 +0100

    Exercicio-02 - Adivinha o número.

--- Exercicios/src/Exercicio02.java
+++ Exercicios/src/Exercicio02.java
@@ -7,13 +7,16 @@ public class Exercicio02 {
 		Scanner teclado = new Scanner(System.in);
 		int tenta = 1, palpite;
 		int num = (int)(Math.random() * 10);
-		System.out.println(num);
+		System.out.println("Vou pensar num número de 0 a 10");
 		
 		while(tenta < 4) {
 			System.out.println("\n-----------------------------");
 			System.out.println("Tentativa " + tenta + " de 3");
 			System.out.print("  ..: Palpite: " );
 			palpite = teclado.nextInt();
+			if(palpite < num) {
+				System.out.println("\nO número que pensei é menor");
+			}
 			
 			tenta++;
 		}
