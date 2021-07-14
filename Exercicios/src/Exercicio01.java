7d0fdfab7d9ecec2e1bff590638334769ce278bf
filipe gaspar <fipewmc2@gmail.com>
2021-07-14 22:57:50 +0100

    Exercicio-01 - Olá Mundo

--- Exercicios/src/Exercicio01.java
+++ Exercicios/src/Exercicio01.java
@@ -4,6 +4,7 @@ public class Exercicio01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá Mundo!");
+		System.out.println("Ufa! Estou livre da maldição.");
 	}
 
 }
