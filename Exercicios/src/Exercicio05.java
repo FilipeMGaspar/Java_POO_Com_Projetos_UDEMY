0b002e2accf264c7fbfc5b9a510482162f9fa771
filipe gaspar <fipewmc2@gmail.com>
2021-07-15 16:49:51 +0100

    Exercício - 05 - Trocar o valor de valor de variáveis

---
+++ Exercicios/src/Exercicio05.java
@@ -0,0 +1,14 @@
+
+public class Exercicio05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Crie um programa que troque os valores 
+		//que estão armazenados nas variáveis nome1 e nome2. 
+		//A variável nome1 armazenara o valor “mestre do universo”. 
+		//A variável nome2 receberá um valor informado pelo usuário.
+		//Altere os valores das variáveis 
+		//e exiba a soma da variável nome1 com o valor da variável nome2.
+	}
+
+}
