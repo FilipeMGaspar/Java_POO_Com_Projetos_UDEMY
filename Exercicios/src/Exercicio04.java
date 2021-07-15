48c93d7e7fc58d686923e5ac78beb7d5a0b5c9f0
filipe gaspar <fipewmc2@gmail.com>
2021-07-15 16:39:45 +0100

    Exercicio 04 - Tabuada

--- Exercicios/src/Exercicio04.java
+++ Exercicios/src/Exercicio04.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Exercicio04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Crie um programa que exiba a tabuada 
+		//de um número informado pelo usuário.
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		
+		teclado.close();
 	}
 
 }
