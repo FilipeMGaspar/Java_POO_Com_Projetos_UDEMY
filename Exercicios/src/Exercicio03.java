f28397d6c3b85737dcc0a96120ac83223920f306
filipe gaspar <fipewmc2@gmail.com>
2021-07-15 08:00:28 +0100

    Exercício 03 - Mensagem personalizada

--- Exercicios/src/Exercicio03.java
+++ Exercicios/src/Exercicio03.java
@@ -3,7 +3,8 @@ public class Exercicio03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// Crie um programa que efetue a leitura de um nome
+		// exiba a mensagem “olá nome informado“.
 	}
 
 }
