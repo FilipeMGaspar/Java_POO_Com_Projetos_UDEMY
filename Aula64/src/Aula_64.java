faf8c71563f68dfc469a85c2ca3797b147bde17e
filipe gaspar <fipewmc2@gmail.com>
2021-07-17 08:27:57 +0100

    Aula 64. Resolvendo um problema sem orientação a objetos

--- Aula64/src/Aula_64.java
+++ Aula64/src/Aula_64.java
@@ -4,6 +4,9 @@ public class Aula_64 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double xA, xB, xC;
+		double yA, yB, yC;
+		
 		Scanner teclado = new Scanner(System.in);
 		teclado.close();
 	}
