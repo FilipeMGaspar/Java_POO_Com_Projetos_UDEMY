6a514aed967a7f275064679254f724ea17572fa7
filipe gaspar <fipewmc2@gmail.com>
2021-07-17 10:56:55 +0100

    Aula 65. Criando uma classe com três atributos para representar melhor… 65. Criando uma classe com três atributos para representar melhor o triângulo

--- Aula65/src/aplication/Aula_65.java
+++ Aula65/src/aplication/Aula_65.java
@@ -1,15 +1,22 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 import entidade.Triangulo;
 
 public class Aula_65 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
 		Triangulo x, y;
 		
 		x = new Triangulo();
 		y = new Triangulo();
+		
+		teclado.close();
 	}
 
 }
