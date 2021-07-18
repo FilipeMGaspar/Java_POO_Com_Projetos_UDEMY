8dc7e7793b2718724f0998be333d3ecb8adf4055
filipe gaspar <fipewmc2@gmail.com>
2021-07-18 21:20:27 +0100

    Aula 67. Começando a resolver um segundo problema exemplo

--- Aula67/src/Aplicacao/Aula_67.java
+++ Aula67/src/Aplicacao/Aula_67.java
@@ -1,19 +1,24 @@
 package Aplicacao;
 
+import java.util.Scanner;
+
 import Entidade.Produto;
 
 public class Aula_67 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		Produto tv = new Produto();
-		tv.AdicionarProduto(1);
-		tv.RemoverProduto(2);
+		//tv.AdicionarProduto(1);
+		//tv.RemoverProduto(2);
 		
 		System.out.println("Criação de produto");
 		System.out.print(" -> Nome ........: ");
 		System.out.print(" -> Preço .......: ");
 		System.out.print(" -> Quantidade ..: ");
+		
+		teclado.close();
 	}
 
 }
