4d00cd23bf885ceb9aee9b377a07075e2a1e6e17
filipe gaspar <fipewmc2@gmail.com>
2021-07-18 21:00:58 +0100

    Aula 67. Começando a resolver um segundo problema exemplo

--- Aula67/src/Aplicacao/Aula_67.java
+++ Aula67/src/Aplicacao/Aula_67.java
@@ -7,6 +7,7 @@ public class Aula_67 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Produto tv = new Produto(); 
+		tv.AdicionarProduto(1);
 		tv.RemoverProduto(2);
 	}
 
