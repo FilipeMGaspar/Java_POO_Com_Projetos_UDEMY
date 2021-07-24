0d17699b2a34d0e33e6b036c8f185fa8f5f458ef
filipe gaspar <fipewmc2@gmail.com>
2021-07-24 10:48:38 +0100

    Aula 70. Exercícios de fixação - Exercício 1

--- Aula70/src/exercicio1/retangulo.java
+++ Aula70/src/exercicio1/retangulo.java
@@ -4,4 +4,12 @@ public class retangulo {
 	//Atributos
 	private double largura;
 	private double comprimento;
+	
+	//Métodos geter e seter
+	public double getLargura() {
+		return this.largura;
+	}
+	public void setLargura(double largura) {
+		this.largura = largura;
+	}
 }
