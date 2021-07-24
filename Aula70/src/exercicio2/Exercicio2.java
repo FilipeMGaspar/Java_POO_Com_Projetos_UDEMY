250177ceec83b757e565bfd6e313cdcf7a5c0f26
filipe gaspar <fipewmc2@gmail.com>
2021-07-24 11:44:28 +0100

    Aula 70. Exercícios de fixação - Exercício 2

--- Aula70/src/exercicio2/Exercicio2.java
+++ Aula70/src/exercicio2/Exercicio2.java
@@ -1,10 +1,20 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		System.out.println(" *** Ficha de empregado *** ");
+		System.out.println();
+		System.out.print();
+		Empregado emp1 = new Empregado();
 		
+		emp1.setNome(null);
+		
+		teclado.close();
 	}
 
 }
