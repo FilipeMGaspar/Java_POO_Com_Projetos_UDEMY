82497e127b2b3e8eedbc33972c32d22561f88d1f
filipe gaspar <fipewmc2@gmail.com>
2021-07-24 10:55:43 +0100

    Aula 70. Exercícios de fixação - Exercício 1

--- Aula70/src/exercicio1/Exercicio1.java
+++ Aula70/src/exercicio1/Exercicio1.java
@@ -4,7 +4,9 @@ public class Exercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println(" *** Retângulos ***");
+		System.out.println();
+		System.out.println("Comprimento do rectângulo? ");
 	}
 
 }
