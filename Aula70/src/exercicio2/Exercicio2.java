8ed8c9e3a19b54fee8c5a723d9fe41de543864bd
filipe gaspar <fipewmc2@gmail.com>
2021-07-24 11:52:59 +0100

    Aula 70. Exercícios de fixação - Exercício 2

--- Aula70/src/exercicio2/Exercicio2.java
+++ Aula70/src/exercicio2/Exercicio2.java
@@ -9,7 +9,9 @@ public class Exercicio2 {
 		Scanner teclado = new Scanner(System.in);
 		System.out.println(" *** Ficha de empregado *** ");
 		System.out.println();
-		System.out.print();
+		System.out.print(" Nome .....: ");
+		System.out.print(" Salário ..: ");
+		System.out.print(" Imposto ..: ");
 		Empregado emp1 = new Empregado();
 		
 		emp1.setNome(null);
