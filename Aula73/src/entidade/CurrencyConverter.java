82c51893d60c1d98df65c3e91d15830db2e6d0fd
filipe gaspar <fipewmc2@gmail.com>
2021-07-26 16:37:44 +0100

    Aula 73. Exercício de fixação

--- Aula73/src/entidade/CurrencyConverter.java
+++ Aula73/src/entidade/CurrencyConverter.java
@@ -4,6 +4,7 @@ public class CurrencyConverter {
 	//Atributos
 	final private double IOF = 6 /100;
 	private double precoDolar;
+	private double qtDolaresComprar;
 	
 	//Métodos geter e Seter
 	private double getPrecoDolar() {
