c4ac0b087a85740450a657959891f3196b630ee3
filipe gaspar <fipewmc2@gmail.com>
2021-07-26 12:33:41 +0100

    Aula 71. Membros estáticos - Parte 1

--- Aula71/src/entidade/Circunferencia.java
+++ Aula71/src/entidade/Circunferencia.java
@@ -1,6 +1,13 @@
 package entidade;
 
 public class Circunferencia {
-	//Atributos
-	private double raio;
+	public final double PI = 3.14159;
+	
+	public double circumference(double radius) {
+		return 2.0 * PI * radius;
+	}
+	
+	public double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+	}
 }
