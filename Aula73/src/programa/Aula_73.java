62b39df854c702ebeadde5ce754a62121dacbf4c
filipe gaspar <fipewmc2@gmail.com>
2021-07-26 16:44:38 +0100

    Aula 73. Exercício de fixação

--- Aula73/src/programa/Aula_73.java
+++ Aula73/src/programa/Aula_73.java
@@ -1,14 +1,23 @@
 package programa;
 
+import java.util.Scanner;
+
 import entidade.CurrencyConverter;
 
 public class Aula_73 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner teclado = new Scanner(System.in);
 		CurrencyConverter dolarReal = new CurrencyConverter();
 		
+		System.out.print("Valor do Dolar: ");
+		dolarReal.setPrecoDolar(teclado.nextDouble());
+		System.out.print("Valor de dolar as comprar: ");
+		dolarReal.setQtDolaresComprar(teclado.nextDouble());
+		
+		System.out.println();
+		teclado.close();
 	}
 
 }
