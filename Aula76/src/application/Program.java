4900f92079e94fea23fd9be4f53954f4deb78451
filipe gaspar <fipewmc2@gmail.com>
2021-07-28 12:38:48 +0100

    Aula 76. Construtores

--- Aula76/src/application/Program.java
+++ Aula76/src/application/Program.java
@@ -32,14 +32,14 @@ public class Program {
 		System.out.println("Product data: " + product);
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();
-		product.addProducts(quantity);
+		int quantit = sc.nextInt();
+		product.addProducts(quantit);
 		System.out.println();
 		System.out.println("Updated data: " + product);
 		System.out.println();
 		System.out.print("Enter the number of products to be removed from stock: ");
-		quantity = sc.nextInt();
-		product.removeProducts(quantity);
+		quantit = sc.nextInt();
+		product.removeProducts(quantit);
 		System.out.println();
 		System.out.println("Updated data: " + product);
 		sc.close();
