e0eb50908421b28d6162c4d8fc8e10b001bf5161
filipe gaspar <fipewmc2@gmail.com>
2021-07-31 14:16:49 +0100

    Aula 82. Exercício de fixação

--- Aula82/src/program/Aula82.java
+++ Aula82/src/program/Aula82.java
@@ -6,8 +6,8 @@ public class Aula82 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ContaNoBanco NBant = new ContaNoBanco(1011, "Alfredo");
-		System.out.println("Olá");
+		ContaNoBanco NBalfredo = new ContaNoBanco(1011, "Alfredo");
+		System.out.println(NBalfredo.toString());
 	}
 
 }
