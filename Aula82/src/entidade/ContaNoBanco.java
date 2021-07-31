87525943cdec96bb1a1be8e88e99e0e6b5ea04d1
filipe gaspar <fipewmc2@gmail.com>
2021-07-31 14:13:23 +0100

    Aula 82. Exercício de fixação

---
+++ Aula82/src/entidade/ContaNoBanco.java
@@ -0,0 +1,39 @@
+package entidade;
+
+public class ContaNoBanco {
+	//Atributos
+	private int numConta;
+	private String nomeTitular;
+	private double valorDepInicial;
+	
+	//Método construtor
+	public ContaBancaria(int numConta, String nomeTitular) {
+		super();
+		this.numConta = numConta;
+		this.nomeTitular = nomeTitular;
+		//this.valorDepInicial = 0; //Opcional
+	}
+	
+	//Métodos Geter e seter
+	public int getNumConta() {
+		return numConta;
+	}
+
+	private void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+	
+	public String getNomeTitular() {
+		return nomeTitular;
+	}
+	public void setNomeTitular(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+	}
+	
+	public double getValorDepInicial() {
+		return valorDepInicial;
+	}
+	public void setValorDepInicial(double valorDepInicial) {
+		this.valorDepInicial = valorDepInicial;
+	}
+}
