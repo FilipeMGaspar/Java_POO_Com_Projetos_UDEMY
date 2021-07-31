64abaea29e2f7f417f16fda705b629e0d861df34
filipe gaspar <fipewmc2@gmail.com>
2021-07-31 14:29:23 +0100

    Aula 82. Exercício de fixação

--- Aula82/src/program/Aula82.java
+++ Aula82/src/program/Aula82.java
@@ -9,8 +9,10 @@ public class Aula82 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);		
+		char resp;
 		int numConta;
 		String nomeTitular;
+		double depInicial;
 		
 		System.out.println(" ?? BANCO DE ENSAIO ?? ");
 		System.out.println();
@@ -18,7 +20,13 @@ public class Aula82 {
 		numConta = teclado.nextInt();
 		System.out.print("  -> Nome do Titular : ");
 		nomeTitular = teclado.nextLine();
+		System.out.print("Deseja fazer um depósito inicial? [S/N] ");
+		resp = teclado.nextLine().charAt(0);
 		
+		if(resp == 'S' || resp == 'S') {
+			System.out.print("Qual o Valor a depositar: ");
+			depInicial = teclado.nextDouble();
+		}
 		
 		//ContaNoBanco NBalfredo = new ContaNoBanco(1011, "Alfredo");
 		//System.out.println(NBalfredo.toString());
