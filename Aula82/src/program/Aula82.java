5be3ae5bb44fb2e92cab9d38e28fe6460fde755a
filipe gaspar <fipewmc2@gmail.com>
2021-07-31 14:21:31 +0100

    Aula 82. Exercício de fixação

--- Aula82/src/program/Aula82.java
+++ Aula82/src/program/Aula82.java
@@ -1,15 +1,22 @@
 package program;
 
+import java.util.Scanner;
+
 import entidade.ContaNoBanco;
 
 public class Aula82 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ContaNoBanco NBalfredo = new ContaNoBanco(1011, "Alfredo");
-		System.out.println(NBalfredo.toString());
+		Scanner teclado = new Scanner(System.in);		
+		int numConta;
 		
-		NBalfredo.setNumConta(200152); //Não será possivel de realizar
+		System.out.println();
+		//ContaNoBanco NBalfredo = new ContaNoBanco(1011, "Alfredo");
+		//System.out.println(NBalfredo.toString());
+		
+		
+		teclado.close();
 	}
 
 }
