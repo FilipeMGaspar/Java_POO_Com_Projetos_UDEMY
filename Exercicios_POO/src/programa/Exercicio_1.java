c43db682ad78284328516f0d00a18728887cb6b0
filipe gaspar <fipewmc2@gmail.com>
2021-08-03 08:24:29 +0100

    Exercícios POO - Exercício 1

--- Exercicios_POO/src/programa/Exercicio_1.java
+++ Exercicios_POO/src/programa/Exercicio_1.java
@@ -1,10 +1,13 @@
 package programa;
 
+import entidades.Aluno;
+
 public class Exercicio_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Aluno joao = new Aluno();
+		
 	}
 
 }
