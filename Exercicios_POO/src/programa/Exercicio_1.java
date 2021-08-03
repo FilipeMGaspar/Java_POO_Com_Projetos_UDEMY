92741bc53a6ce6d5f8389823a6bdbfad2a4d9170
filipe gaspar <fipewmc2@gmail.com>
2021-08-03 08:39:26 +0100

    Exercícios POO - Exercício 1

--- Exercicios_POO/src/programa/Exercicio_1.java
+++ Exercicios_POO/src/programa/Exercicio_1.java
@@ -25,6 +25,8 @@ public class Exercicio_1 {
 		System.out.println(" > NOME ............: " + joao.getNome());
 		System.out.println(" > Email ...........: " + joao.getEmail());
 	
+		System.out.println("\n");
+		System.out.println(" ## Ficha de Aluno ##\n");
 		//System.out.println(joao.toString());
 		teclado.close();
 	}
