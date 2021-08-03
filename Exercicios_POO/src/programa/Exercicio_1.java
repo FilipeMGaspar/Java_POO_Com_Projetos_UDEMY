f46dd5b4001ad11767982179c270ab01a3adf849
filipe gaspar <fipewmc2@gmail.com>
2021-08-03 08:27:10 +0100

    Exercícios POO - Exercício 1

--- Exercicios_POO/src/programa/Exercicio_1.java
+++ Exercicios_POO/src/programa/Exercicio_1.java
@@ -1,13 +1,18 @@
 package programa;
 
+import java.util.Scanner;
+
 import entidades.Aluno;
 
 public class Exercicio_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		Aluno joao = new Aluno();
+		System.out.println("*** Ficha de inscrição ***");
 		
+		teclado.close();
 	}
 
 }
