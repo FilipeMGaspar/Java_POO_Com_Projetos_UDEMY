0b8c3248315c2318b185e524e59bbf3f580c22d8
filipe gaspar <fipewmc2@gmail.com>
2021-08-03 08:22:17 +0100

    Exercícios POO - Exercício 1

--- Exercicios_POO/src/entidades/Aluno.java
+++ Exercicios_POO/src/entidades/Aluno.java
@@ -5,4 +5,25 @@ public class Aluno {
 	private long matricula;
 	private String nome;
 	private String email;
+	
+	//Métodos Geter e Seter
+	public long getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(long matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
