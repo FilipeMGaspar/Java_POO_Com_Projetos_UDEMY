a672eaafe20c08d3736c39c95dba83691664b0a4
filipe gaspar <fipewmc2@gmail.com>
2021-08-04 11:57:11 +0100

    Exercícios POO - Exercício 2

--- Exercicios_POO/src/entidades/Disciplina.java
+++ Exercicios_POO/src/entidades/Disciplina.java
@@ -6,4 +6,24 @@ public class Disciplina {
 	private int cargaHoraria;
 	private String codigo;
 	
+	//Métodos Geter e Seter
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
 }
