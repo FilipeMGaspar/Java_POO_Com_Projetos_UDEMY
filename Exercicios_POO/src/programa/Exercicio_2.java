46efec8e1bddc933879cf9a3c44c77c04aff1464
filipe gaspar <fipewmc2@gmail.com>
2021-08-04 12:02:33 +0100

    Exercícios POO - Exercício 2

--- Exercicios_POO/src/programa/Exercicio_2.java
+++ Exercicios_POO/src/programa/Exercicio_2.java
@@ -1,10 +1,13 @@
 package programa;
 
+import entidades.Disciplina;
+
 public class Exercicio_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Disciplina mat = new Disciplina();
+		
 	}
 
 }
