46ef5319be3354d2355210f16439986c3e5dbde4
filipe gaspar <fipewmc2@gmail.com>
2021-08-04 12:03:16 +0100

    Exercícios POO - Exercício 2

--- Exercicios_POO/src/programa/Exercicio_2.java
+++ Exercicios_POO/src/programa/Exercicio_2.java
@@ -1,13 +1,17 @@
 package programa;
 
+import java.util.Scanner;
+
 import entidades.Disciplina;
 
 public class Exercicio_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		Disciplina mat = new Disciplina();
 		
+		teclado.close();
 	}
 
 }
