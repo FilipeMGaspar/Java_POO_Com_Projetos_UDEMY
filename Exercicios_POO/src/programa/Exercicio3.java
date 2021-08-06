3b165035cb5e4abfc96d5485c199b5a52ffa8908
filipe gaspar <fipewmc2@gmail.com>
2021-08-06 08:46:27 +0100

    Exercícios POO - Exercício 3

--- Exercicios_POO/src/programa/Exercicio3.java
+++ Exercicios_POO/src/programa/Exercicio3.java
@@ -1,12 +1,17 @@
 package programa;
 
+import java.util.Scanner;
+
 import entidades.Professor;
 
 public class Exercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		Professor prof = new Professor();
+		
+		teclado.close();
 	}
 
 }
