141218348de9f04de109ebcf5bb0649ec49e4a1e
filipe gaspar <fipewmc2@gmail.com>
2021-08-08 08:41:47 +0100

    Exercícios POO - Exercício 4

--- Exercicios_POO/src/entidades/Funcionario.java
+++ Exercicios_POO/src/entidades/Funcionario.java
@@ -7,4 +7,37 @@ public class Funcionario {
 	private int idade;
 	private String cargo;
 	private double salario;
+	
+	//Métodos geter e seter
+	public int getRegisto() {
+		return registo;
+	}
+	public void setRegisto(int registo) {
+		this.registo = registo;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
 }
