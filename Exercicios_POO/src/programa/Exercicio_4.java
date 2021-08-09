07d1db6b0f4cdb4cdd9e4b4747954cdd3f084aba
filipe gaspar <fipewmc2@gmail.com>
2021-08-09 10:57:47 +0100

    Exercícios POO - Exercício 4

--- Exercicios_POO/src/programa/Exercicio_4.java
+++ Exercicios_POO/src/programa/Exercicio_4.java
@@ -1,13 +1,17 @@
 package programa;
 
+import java.util.Scanner;
+
 import entidades.Funcionario;
 
 public class Exercicio_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
 		Funcionario func1 = new Funcionario();
 		
+		teclado.close();
 	}
 
 }
