dd5dd5269837f02a0b887c6920c2d7cea748e1ea
filipe gaspar <fipewmc2@gmail.com>
2021-08-09 10:56:05 +0100

    Exercícios POO - Exercício 4

--- Exercicios_POO/src/programa/Exercicio_4.java
+++ Exercicios_POO/src/programa/Exercicio_4.java
@@ -1,10 +1,13 @@
 package programa;
 
+import entidades.Funcionario;
+
 public class Exercicio_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Funcionario func1 = new Funcionario();
+		
 	}
 
 }
