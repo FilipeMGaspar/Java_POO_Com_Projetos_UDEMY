d23db378c4fcbf16ca2aeb759951ae6b8ba7f7d4
filipe gaspar <fipewmc2@gmail.com>
2021-08-10 08:48:41 +0100

    Aula 87. Tipos referência vs. tipos valor

--- Aula87/src/programa/Aula_87.java
+++ Aula87/src/programa/Aula_87.java
@@ -4,7 +4,10 @@ public class Aula_87 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int p;
+		System.out.println(p); // erro: variável não iniciada
+		p = 10;
+		System.out.println(p);
 	}
 
 }
