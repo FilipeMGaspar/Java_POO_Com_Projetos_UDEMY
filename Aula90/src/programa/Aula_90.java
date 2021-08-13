a277965295787125f75b1e58cfe6bbca4dcff29b
filipe gaspar <fipewmc2@gmail.com>
2021-08-13 13:22:14 +0100

    Aula 90. Vetores - Parte 2

--- Aula90/src/programa/Aula_90.java
+++ Aula90/src/programa/Aula_90.java
@@ -1,10 +1,14 @@
 package programa;
 
+import java.util.Scanner;
+
 public class Aula_90 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		
+		teclado.close();
 	}
 
 }
