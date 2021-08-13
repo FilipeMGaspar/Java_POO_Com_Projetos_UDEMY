bb3e6d27d9cd4c1d434930595d2bd1009d91fe3e
filipe gaspar <fipewmc2@gmail.com>
2021-08-13 13:24:35 +0100

    Aula 90. Vetores - Parte 2

--- Aula90/src/programa/Aula_90.java
+++ Aula90/src/programa/Aula_90.java
@@ -7,6 +7,10 @@ public class Aula_90 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
+		int n;
+		
+		System.out.print("Quantos produtos deseja registar: ");
+		n = teclado.nextInt();
 		
 		teclado.close();
 	}
