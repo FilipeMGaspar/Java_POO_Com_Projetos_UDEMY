de5e78ab1b2ba628aaee27947e63d4fea4c164ed
filipe gaspar <fipewmc2@gmail.com>
2021-08-13 14:00:37 +0100

    Aula 90. Vetores - Parte 2

--- Aula90/src/entidade/Profutos.java
+++ Aula90/src/entidade/Profutos.java
@@ -4,6 +4,10 @@ public class Profutos {
 	//Atributos
 	private String nome;
 	private double preco;
-	
-	
+		
+	//Método construtor
+	public Profutos(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
 }
