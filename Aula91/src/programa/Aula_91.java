7e649973357ece0d4a79166760521bfbbfb45822
filipe gaspar <fipewmc2@gmail.com>
2021-08-14 10:28:28 +0100

    Aula 91. Exercício de fixação

--- Aula91/src/programa/Aula_91.java
+++ Aula91/src/programa/Aula_91.java
@@ -2,6 +2,8 @@ package programa;
 
 import java.util.Scanner;
 
+import entidade.Alugar;
+
 public class Aula_91 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,10 @@ public class Aula_91 {
 		Scanner teclado = new Scanner(System.in);
 		int qtAlugar;
 		
+		Alugar quartos[] = new Alugar[10];  //vetor com 10 quartos
+		
+		System.out.println(" *** ALuguer de Quartos ***");
+		System.out.println();
 		System.out.println();
 		
 		teclado.close();
