0f5b43d90cd5c197ca1e010e74ed6ca45dae6604
filipe gaspar <fipewmc2@gmail.com>
2021-08-14 10:27:12 +0100

    Aula 91. Exercício de fixação

--- Aula91/src/programa/Aula_91.java
+++ Aula91/src/programa/Aula_91.java
@@ -1,10 +1,17 @@
 package programa;
 
+import java.util.Scanner;
+
 public class Aula_91 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		int qtAlugar;
+		
+		System.out.println();
+		
+		teclado.close();
 	}
 
 }
