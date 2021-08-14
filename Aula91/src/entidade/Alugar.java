49ba1efab4f254a0f9f32662b979f971300e1bf0
filipe gaspar <fipewmc2@gmail.com>
2021-08-14 10:24:37 +0100

    Aula 91. Exercício de fixação

--- Aula91/src/entidade/Alugar.java
+++ Aula91/src/entidade/Alugar.java
@@ -4,6 +4,18 @@ public class Alugar {
 	//Atributos
 	private String nome;
 	private String email;
-	
-	
+
+	//Métodos geter e seter
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
