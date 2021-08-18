e453ee63d9d42eb7ff3de1ebfaf6e366e2166cc6
filipe gaspar <fipewmc2@gmail.com>
2021-08-18 13:52:09 +0100

    Aula - 92. Boxing, unboxing e wrapper classes

--- Aula92/src/programa/Aula_92.java
+++ Aula92/src/programa/Aula_92.java
@@ -4,7 +4,13 @@ public class Aula_92 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int x = 20;
+		
+		Object obj = x;
+		
+		System.out.println(obj);
+		
 	}
 
 }
