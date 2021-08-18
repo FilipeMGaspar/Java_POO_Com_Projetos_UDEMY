cdee53bf0ec632188e49f6a91c363deb18e5c943
filipe gaspar <fipewmc2@gmail.com>
2021-08-18 14:21:52 +0100

    Aula 94. Listas - Parte 1

--- Aula94/src/programa/Aula_94.java
+++ Aula94/src/programa/Aula_94.java
@@ -1,10 +1,13 @@
 package programa;
 
+import com.sun.tools.javac.util.List;
+
 public class Aula_94 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		List x = new List();
+		
 	}
 
 }
