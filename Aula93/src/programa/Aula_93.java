caa10a28ded585d379f543ff33bacdc2b1bc9e99
filipe gaspar <fipewmc2@gmail.com>
2021-08-18 14:01:35 +0100

    Aula - 93. Laço for each

--- Aula93/src/programa/Aula_93.java
+++ Aula93/src/programa/Aula_93.java
@@ -4,7 +4,15 @@ public class Aula_93 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] vect = new String[] {"Maria", "Bob", "Alex"};
+		
+		for (int i=0; i< vect.length; i++) {
+			System.out.println(vect[i]);
+		}
+		
+		for (String obj : vect) { // Laço for each 
+			System.out.println(obj);
+		}
 	}
 
 }
