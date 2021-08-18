409aed6bcb2c6c2b74314c65cfdbae7da1b4c312
filipe gaspar <fipewmc2@gmail.com>
2021-08-18 14:22:57 +0100

    Aula 94. Listas - Parte 1

--- Aula94/src/programa/Aula_94.java
+++ Aula94/src/programa/Aula_94.java
@@ -1,12 +1,12 @@
 package programa;
 
-import com.sun.tools.javac.util.List;
+//import com.sun.tools.javac.util.List;
 
 public class Aula_94 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List x = new List();
+		//List x = new List(); //Tem de ser implementada uma interface através de uma classe
 		
 	}
 
