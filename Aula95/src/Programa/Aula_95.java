b108114a4771f0b3147fec4929a024b168cb4dda
filipe gaspar <fipewmc2@gmail.com>
2021-08-20 08:23:23 +0100

    Aula 95. Listas - Parte 2

--- Aula95/src/Programa/Aula_95.java
+++ Aula95/src/Programa/Aula_95.java
@@ -8,6 +8,12 @@ public class Aula_95 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<String> list = new ArrayList();;
+		
+		list.add("Ana");
+		list.add("Manuel");
+		list.add("Maria");
+		list.add("Bob");
+		
 	}
 
 }
