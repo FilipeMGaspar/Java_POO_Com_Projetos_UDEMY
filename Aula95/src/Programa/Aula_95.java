33b2605b21373e8cb52fcff59866f759226ce736
filipe gaspar <fipewmc2@gmail.com>
2021-08-20 08:22:26 +0100

    Aula 95. Listas - Parte 2

--- Aula95/src/Programa/Aula_95.java
+++ Aula95/src/Programa/Aula_95.java
@@ -1,10 +1,13 @@
 package Programa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aula_95 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<String> list = new List<>();
+		List<String> list = new ArrayList();;
 	}
 
 }
