b18f155d1b69ca544732d7c004e48a342088dbcf
filipe gaspar <fipewmc2@gmail.com>
2021-08-22 11:02:52 +0100

    Aula 96. Exercício proposto

--- Aula96/src/entidade/Empregado.java
+++ Aula96/src/entidade/Empregado.java
@@ -12,6 +12,31 @@ public class Empregado {
 		Nome = nome;
 		this.salario = salario;
 	}
+
+	//Métodos Geter e seter
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
 	
 	
 }
