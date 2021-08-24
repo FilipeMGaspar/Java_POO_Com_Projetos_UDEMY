d933c08b92dac73ed552fdb69a3fb1d30e9ef977
filipe gaspar <fipewmc2@gmail.com>
2021-08-24 21:51:22 +0100

    Aula 96. Exercício proposto

--- Aula96/src/programa/Aula_96.java
+++ Aula96/src/programa/Aula_96.java
@@ -1,13 +1,19 @@
 package programa;
 
+import java.util.Scanner;
+
 public class Aula_96 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner teclado = new Scanner(System.in);
 		int numEmp;
 		
 		System.out.println(" ..: Gestão de Funcionários :.. \n");
 		System.out.print(" Quantos funcionários deseja registar: ");
+		
+		teclado.close();
 	}
 
 }
