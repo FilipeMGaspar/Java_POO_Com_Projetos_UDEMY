465833ad8f99c628ac11fb964af283146bfd0b0e
FilipeMGaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-06 10:40:50 +0100

    Aula 99 - Exercício resolvido Matrizes

--- Aula99/src/aula99/Aula_99.java
+++ Aula99/src/aula99/Aula_99.java
@@ -1,10 +1,14 @@
 package aula99;
 
+import java.util.Scanner;
+
 public class Aula_99 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
 	}
 
 }
